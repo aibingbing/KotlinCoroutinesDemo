f2db6aa86fe2373e7c6e554deeea4a7e7c90956f
aibingbing <aibingbing@zhidaoauto.com>
2023-08-14 19:31:21 +0800

    feat: coroutine exception;

--- src/CoroutineException.kt
+++ src/CoroutineException.kt
@@ -24,26 +24,26 @@ import kotlin.coroutines.suspendCoroutine
 
 //----------------------------------------------------------
 
-interface Callback<T> {
-    fun onSuccess(t: T)
-    fun onError(t: Throwable)
-}
-
-fun getCount(callback: Callback<Int>) {
-
-}
-
-suspend fun getCountCoroutine() = suspendCoroutine<Int> { continuation ->
-    getCount(object : Callback<Int> {
-        override fun onSuccess(t: Int) {
-            continuation.resume(t)
-        }
-
-        override fun onError(t: Throwable) {
-            continuation.resumeWithException(t)
-        }
-    })
-}
+//interface Callback<T> {
+//    fun onSuccess(t: T)
+//    fun onError(t: Throwable)
+//}
+//
+//fun getCount(callback: Callback<Int>) {
+//
+//}
+//
+//suspend fun getCountCoroutine() = suspendCoroutine<Int> { continuation ->
+//    getCount(object : Callback<Int> {
+//        override fun onSuccess(t: Int) {
+//            continuation.resume(t)
+//        }
+//
+//        override fun onError(t: Throwable) {
+//            continuation.resumeWithException(t)
+//        }
+//    })
+//}
 
 //val  loadCount = {
 //    GlobalScope.launch (Dispatchers.Main){
@@ -74,14 +74,88 @@ suspend fun getCountCoroutine() = suspendCoroutine<Int> { continuation ->
 //    log(2)
 //}
 
+//suspend fun main() {
+//    log(1)
+//    try {
+//        supervisorScope {
+//            log(2)
+//            launch {
+//                log(3)
+//                launch {
+//                    log(4)
+//                    delay(100)
+//                    throw  ArithmeticException("Hey!")
+//                }
+//                log(5)
+//            }
+//            log(6)
+//            val job = launch {
+//                log(7)
+//                delay(1000)
+//            }
+//            try {
+//                log(8)
+//                job.join()
+//                log(9)
+//            } catch (e: Exception) {
+//                log("10 $e")
+//            }
+//        }
+//        log(11)
+//    } catch (e: Exception) {
+//        log("12 $e")
+//    }
+//    log(13)
+//}
+
+//suspend fun main() {
+//    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+//        log("Throws an exception with message: ${throwable.message}")
+//    }
+//    log(1)
+//    try {
+//        coroutineScope {
+//            log(2)
+//            launch {
+//                log(3)
+//                launch(exceptionHandler) {
+//                    log(4)
+//                    delay(100)
+//                    throw  ArithmeticException("Hey!")
+//                }
+//                log(5)
+//            }
+//            log(6)
+//            val job = launch {
+//                log(7)
+//                delay(1000)
+//            }
+//            try {
+//                log(8)
+//                job.join()
+//                log(9)
+//            } catch (e: Exception) {
+//                log("10 $e")
+//            }
+//        }
+//        log(11)
+//    } catch (e: Exception) {
+//        log("12 $e")
+//    }
+//    log(13)
+//}
+
 suspend fun main() {
+    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        log("Throws an exception with message: ${throwable.message}")
+    }
     log(1)
     try {
         supervisorScope {
             log(2)
             launch {
                 log(3)
-                launch {
+                launch(exceptionHandler) {
                     log(4)
                     delay(100)
                     throw  ArithmeticException("Hey!")
